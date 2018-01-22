e4c35d715b3b1685679c9845a9368ab72c6ba85d
Carlton <carltonfoo@gmail.com>
2018-01-22 15:03:31 +0800

    added vas's files

--- src/preprocess/dataCleaner.js
+++ src/preprocess/dataCleaner.js
@@ -1,17 +1,26 @@
 import fs from 'fs'
 
+const DAYS_OF_WEEK = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+  'Public Holiday'
+]
+
 const files = fs.readdirSync('data/raw').filter(file => file.match(/\.json$/))
 const locations = require('../../data/locations.json')
 
-const daysArr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Public Holiday']
-
 files.forEach(file => {
   try {
-    cleanOperatingHours
     const raw = require('../../data/raw/' + file)
+
     const processed = Object.assign({}, raw)
+
     processed.id = raw.hciCode
-    delete processed.hciCode
 
     if (!raw.licenseClass) {
       const [licensePeriod, licenseClass] = raw.licensePeriod.split(/[\n\t]+/)
@@ -24,7 +33,6 @@ files.forEach(file => {
     }
 
     const location = locations[processed.id]
-    processed.location = location
     Object.assign(processed, location)
 
     fs.writeFileSync(`public/data/entities/${processed.id}.json`, JSON.stringify(processed, null, '\t'))
@@ -37,7 +45,7 @@ files.forEach(file => {
 function cleanOperatingHours (input) {
   const output = {}
   Object.keys(input).forEach(key => {
-    const value = input[key]
+    const value = cleanOperatingTime(input[key])
     let splitted = key.split(' and ')
     if (splitted.length > 1) {
       splitted.forEach(day => {
@@ -46,10 +54,10 @@ function cleanOperatingHours (input) {
     } else {
       splitted = splitted[0].split(' to ')
       if (splitted.length > 1) {
-        const indexStart = daysArr.indexOf(splitted[0])
-        const indexEnd = daysArr.indexOf(splitted[1])
+        const indexStart = DAYS_OF_WEEK.indexOf(splitted[0])
+        const indexEnd = DAYS_OF_WEEK.indexOf(splitted[1])
         for (let i = indexStart; i <= indexEnd; i++) {
-          output[daysArr[i]] = value
+          output[DAYS_OF_WEEK[i]] = value
         }
       } else {
         output[key] = value
@@ -57,4 +65,51 @@ function cleanOperatingHours (input) {
     }
   })
   return output
+}
+
+function cleanOperatingTime (input) {
+  const output = []
+  let splitPeriods = input.split(', ')
+  if (splitPeriods.length > 1) {
+    splitPeriods.forEach(time => {
+      const frame = []
+      let splitted = time.split(' to ')
+      if (splitted.length > 1) {
+        splitted.forEach(time => {
+          frame.push(transformTime(time))
+        })
+      }
+      output.push(frame)
+    })
+  } else {
+    if (splitPeriods[0] === 'Closed') {
+      // do nothing
+    } else {
+      const frame = []
+      let splitted = splitPeriods[0].split(' to ')
+      if (splitted.length > 1) {
+        splitted.forEach(time => {
+          frame.push(transformTime(time))
+        })
+      }
+      output.push(frame)
+    }
+  }
+  return output
+}
+
+function transformTime (input) {
+  console.log(input)
+  const inputArr = input.slice(1, 6).split(':')
+  const hour = parseInt(inputArr[0])
+  const min = parseInt(inputArr[1])
+  if (input.indexOf('am') > 1) {
+    console.log(('0' + hour).slice(-2) + ':' + ('0' + min).slice(-2))
+    return (('0' + hour).slice(-2) + ':' + ('0' + min).slice(-2))
+  } else if (input.indexOf('pm') > 1) {
+    console.log(('0' + (hour + 12)).slice(-2) + ':' + ('0' + min).slice(-2))
+    return (('0' + (hour + 12)).slice(-2) + ':' + ('0' + min).slice(-2))
+  } else {
+    console.log('Err: No valid time to transform')
+  }
 }
